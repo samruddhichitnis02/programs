5391f08c16cae58d37a3d7505b3dd07f48d5a789
samruddhichitnis02 <samruddhichitnis02@gmail.com>
2019-06-28 14:41:25 +0530

    A D F

--- ProgramsA/Utilities/data.py
+++ ProgramsA/Utilities/data.py
@@ -272,10 +272,16 @@ def cal(y,m):
     b = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
     for i in range(0, len(b)):
         a[i].append(b[i])
-    p = 1
-    d = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
 
-    d0 = int((y + y / 4 - y / 100 + y / 400 + d[m - 1] + p) % 7)
+    if(((y%4==0)and(y%100!=0)) or (y%400==0)):
+        p = 1
+        d =[6, 9, 3, 6, 1, 4, 6, 2, 5, 7, 3, 5]
+        d0 = int((y + y / 4 - y / 100 + y / 400 + d[m - 1] + p) % 7)
+    else:
+        p=1
+        d = [0, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+        d0 = int((y + y / 4 - y / 100 + y / 400 + d[m - 1] + p) % 7)
+
     if (d0 == 0):
         j = 0
         if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
@@ -286,6 +292,13 @@ def cal(y,m):
                     j = 0
         elif (m == 11 or m == 4 or m == 6 or m == 9):
             for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+        elif(m==2):
+            for k in range(1, 29):
                 a[j].append(k)
                 j = j + 1
                 if (j == 7):
@@ -302,6 +315,13 @@ def cal(y,m):
                     j = 0
         if (m == 11 or m == 4 or m == 6 or m == 9):
             for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+        elif (m == 2):
+            for k in range(1, 29):
                 a[j].append(k)
                 j = j + 1
                 if (j == 7):
@@ -324,6 +344,13 @@ def cal(y,m):
                 if (j == 7):
                     j = 0
 
+        elif (m == 2):
+            for k in range(1, 29):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
     elif (d0 == 3):
         j = 3
         if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
@@ -339,6 +366,14 @@ def cal(y,m):
                 if (j == 7):
                     j = 0
 
+        elif (m == 2):
+            for k in range(1, 29):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+
     elif (d0 == 4):
         j = 4
         if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
@@ -347,12 +382,20 @@ def cal(y,m):
                 j = j + 1
                 if (j == 7):
                     j = 0
-        if (m == 11 or m == 4 or m == 6 or m == 9):
+        elif (m == 11 or m == 4 or m == 6 or m == 9):
             for k in range(1, 31):
                 a[j].append(k)
                 j = j + 1
                 if (j == 7):
                     j = 0
+
+        elif (m == 2):
+            for k in range(1, 29):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
 
     elif (d0 == 5):
         j = 5
@@ -362,8 +405,15 @@ def cal(y,m):
                 j = j + 1
                 if (j == 7):
                     j = 0
-        if (m == 11 or m == 4 or m == 6 or m == 9):
+        elif (m == 11 or m == 4 or m == 6 or m == 9):
             for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+        elif (m == 2):
+            for k in range(1, 29):
                 a[j].append(k)
                 j = j + 1
                 if (j == 7):
@@ -378,12 +428,21 @@ def cal(y,m):
                 j = j + 1
                 if (j == 7):
                     j = 0
-        if (m == 11 or m == 4 or m == 6 or m == 9):
+        elif (m == 11 or m == 4 or m == 6 or m == 9):
             for k in range(1, 31):
                 a[j].append(k)
                 j = j + 1
                 if (j == 7):
                     j = 0
+
+
+        elif (m == 2):
+            for k in range(1, 29):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
 
     if (m == 1):
         print('January', y)
