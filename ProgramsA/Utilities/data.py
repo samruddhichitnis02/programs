fdd1d27e6fcf646543702dbd62e0ec6470bb403a
samruddhichitnis02 <samruddhichitnis02@gmail.com>
2019-06-28 13:13:06 +0530

    F & A & D

--- ProgramsA/Utilities/data.py
+++ ProgramsA/Utilities/data.py
@@ -263,3 +263,150 @@ def anagram_2D():
     for i in range(0,2):
         a.append([])
     return a
+
+#Calender
+def cal(y,m):
+    a=[ ]
+    for i in range(0, 7):
+        a.append([])
+    b = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
+    for i in range(0, len(b)):
+        a[i].append(b[i])
+    p = 1
+    d = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+
+    d0 = int((y + y / 4 - y / 100 + y / 400 + d[m - 1] + p) % 7)
+    if (d0 == 0):
+        j = 0
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+            for k in range(1, 32):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+        elif (m == 11 or m == 4 or m == 6 or m == 9):
+            for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+
+    elif (d0 == 1):
+        j = 1
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+            for k in range(1, 32):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+        if (m == 11 or m == 4 or m == 6 or m == 9):
+            for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+
+
+    elif (d0 == 2):
+        j = 2
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+            for k in range(1, 32):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+        if (m == 11 or m == 4 or m == 6 or m == 9):
+            for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+    elif (d0 == 3):
+        j = 3
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+            for k in range(1, 32):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+        if (m == 11 or m == 4 or m == 6 or m == 9):
+            for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+    elif (d0 == 4):
+        j = 4
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+            for k in range(1, 32):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+        if (m == 11 or m == 4 or m == 6 or m == 9):
+            for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+    elif (d0 == 5):
+        j = 5
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+            for k in range(1, 32):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+        if (m == 11 or m == 4 or m == 6 or m == 9):
+            for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+
+    elif (d0 == 6):
+        j = 6
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+            for k in range(1, 32):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+        if (m == 11 or m == 4 or m == 6 or m == 9):
+            for k in range(1, 31):
+                a[j].append(k)
+                j = j + 1
+                if (j == 7):
+                    j = 0
+
+    if (m == 1):
+        print('January', y)
+    elif (m == 2):
+        print('February', y)
+    elif (m == 3):
+        print('March', y)
+    elif (m == 4):
+        print('April', y)
+    elif (m == 5):
+        print('May', y)
+    elif (m == 6):
+        print('June', y)
+    elif (m == 7):
+        print('July', y)
+    elif (m == 8):
+        print('August', y)
+    elif (m == 9):
+        print('September', y)
+    elif (m == 10):
+        print('October', y)
+    elif (m == 11):
+        print('November', y)
+    else:
+        print('December', y)
+    return a
